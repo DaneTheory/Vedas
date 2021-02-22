42a79dfb95a61c3b9c23357aa1a3c2ecc8055cf6
Branden Dane <DaneTheory@gmail.com>
2021-02-22 00:58:05 -0500

    fixed jest config

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,10 @@
-const vedas = require('../dist')
+const vedas = require('../dist/index.js')
 
-console.log(vedas)
+// console.log(vedas())
 
 test('should resolve the lib\'s top-level export', () => {
   const actual = vedas()
+
   expect(actual).toBeDefined()
+  expect(actual).toBe('Youve been, THUNDERSTRUCk')
 })
